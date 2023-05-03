df4513d4045333265acdb5e08b21cf8c2fdfa920
Rami Nakkar <rami.nakkar@miragon.io>
2023-05-03 20:09:55 +0200

    add html and styles

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
 
+  const safe = null;
+  const last_query = null;
+
   async function onSubmit(event) {
     event.preventDefault();
     try {
@@ -32,27 +35,45 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+      <div>
+        <Head>
+          <title>Football Genie</title>
+        </Head>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
+      <div className={styles.background}></div>
+        <div className={styles.container}>
+          <div id={styles.headerContainer}>
+
+            <div id={styles.imageDiv}>
+              <img src="/football.png" className={styles.smallImage}  alt="screaming-man"/>
+              <h4>Name the Icon</h4>
+            </div>
+
+            <form id="query-form" action="/" method="post">
+              <input type="text" name="animal" placeholder="Ask football questions" required/>
+              <input type="submit" value="Search"/>
+            </form>
+
+          </div>
+
+          {
+            last_query &&
+            <div className="last-query">
+                Last query: { last_query }
+            </div>
+          }
+
+          {
+            <div className="result">
+              <div id="result-div">
+                {
+                  result | safe
+                }
+              </div>
+            </div>
+          }
+
+        </div>
+      </div>
   );
 }
